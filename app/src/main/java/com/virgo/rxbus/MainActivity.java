69826559805f8e78e01c3fcb9fea5b1bd0fb25f6
gaoguanqi <307590625@qq.com>
2017-03-22 14:47:06 +0800

    RxRecyclerView1

--- app/src/main/java/com/virgo/rxbus/MainActivity.java
+++ app/src/main/java/com/virgo/rxbus/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import com.virgo.rxbus.base.BaseActivity;
 import com.virgo.rxbus.test.EventSendActivity;
 import com.virgo.rxbus.test.PhotoViewActivity;
+import com.virgo.rxbus.test.RecyclerViewActivity;
 
 public class MainActivity extends BaseActivity {
 
@@ -25,6 +26,12 @@ public class MainActivity extends BaseActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, PhotoViewActivity.class));
+            }
+        });
+        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RecyclerViewActivity.class));
             }
         });
     }
